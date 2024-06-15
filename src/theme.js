99c4cccfef4a220d2be6151398bd4b5ed95a1719
Jonasz Jachym <jajonaszjj@gmail.com>
2024-06-15 20:39:09 +0200

    comments removed

--- src/theme.js
+++ src/theme.js
@@ -4,23 +4,16 @@ const breakpoint = {
 };
 
 export const theme = {
-  breakpoint: {
-    mobile: breakpoint.mobile,
-    tablet: breakpoint.tablet,
-  },
+  breakpoint,
   color: {
     background: "#FBFBFE",
     elementBackground: "#FFFFFF",
     mainAccent: "#0366D6",
     secondaryAccent: "#252525",
-    // mainBlueDM: "#2188FF",
     buttonText: "#FFFFFF",
     textSecondary: "#6E7E91",
-    // lightModeBG: "#FBFBFE",
     borderNeutral: "rgba(209, 213, 218, 0.3)",
     portfolioBorder: "#E5E5E5",
-    // semiGrey: "rgba(209, 213, 218, 0.1)",
-    // semiDark: "rgba(54, 54, 54, 0.7)",
   },
 };
 
@@ -34,13 +27,9 @@ export const darkTheme = {
     elementBackground: "#363636",
     mainAccent: "#2188FF",
     secondaryAccent: "#FFFFFF",
-    // mainBlueDM: "#2188FF",
     buttonText: "#FFFFFF",
     textSecondary: "#FFFFFF",
-    // lightModeBG: "#FBFBFE",
     borderNeutral: "rgba(209, 213, 218, 0.3)",
     portfolioBorder: "rgba(54, 54, 54, 0.7)",
-    // semiGrey: "rgba(209, 213, 218, 0.1)",
-    // semiDark: "rgba(54, 54, 54, 0.7)",
   },
 };
